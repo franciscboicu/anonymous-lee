8a87c0c008793693d96e1cc386a5a728f010f7d8
Gheboianu Bogdan <bogdan.gheboianu.2013@gmail.com>
2020-03-28 23:55:32 +0200

    Added: session log in

--- user_data.py
+++ user_data.py
@@ -1,5 +1,14 @@
-import db, bcrypt, hashlib
+import db, bcrypt, hashlib, os
+from flask import session
 
+def random_api_key():
+    return os.urandom(100)
+
+
+def is_logged_in():
+    if 'logged_in' in session:
+        return True
+    return False
 
 @db.use
 def register_user(cursor, username, text_password):
